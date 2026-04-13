ce5ca81949310314ba24333da2eb1ca9a63cc689
swappel <sebastian.lauber38@gmail.com>
2026-04-13 17:44:29 +0200

    Adjusted some small details in CSS (still change box-size for discord stats).
    Still need to complete the index.js with the json parsing and putting that into the page as cards.

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,28 @@
 // TODO: Implement dark light detection
 // TODO: Make cards from JSON work
 
+async function loadJSONFiles(){
+    // Fetch both JSON files and only then continue
+    const [cardList, tagList] = await Promise.all([
+        fetch('./src/json/link-cards.json'),
+        fetch('./src/json/link-card-tags.json')
+    ]);
+
+    // Get the file's JSON data
+    const cardsData = await cardList.json();
+    const tagsData = await tagList.json();
+
+    console.log(`cardData: ${cardsData}\ntagsData: ${tagsData}`);
+
+    // Launch the parser function
+    parseJSONData(cardsData, tagsData);
+}
+
+
+function parseJSONData(cards, tags) {
+    cards.link_cards.forEach(card => {
+       console.log(`Card title: ${card.title}`);
+    });
+}
+
+loadJSONFiles();
